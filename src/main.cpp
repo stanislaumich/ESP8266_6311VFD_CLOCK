31170ba5d73c08385701cd2cf6b0c42e5bcae34b
Stas <staslz42stas@gmail.com>
2019-11-22 14:24:45 +0300

    вычистил IR

--- src/main.cpp
+++ src/main.cpp
@@ -23,23 +23,33 @@
 
 /////////////////////////////////////////////////////////////////////////////////////// 
 void setup() {
-  initCommon();
-  beep(125,50);
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("Booting Sketch...");
+  mLog(" ");    
+  mLog("Booting...");
+  mLog("Starting common...");
+  initCommon();    
+  mLog("Beep...");
+  beep(125,50);
+  mLog("IR...");
+  MyIRInit();
+  mLog("WiFi...");
   MyWiFiInit();
+  mLog("Time...");
   MyTimeInit();
+  mLog("Web...");
   MyWebinit();
+  mLog("TeleBot...");
   MyTeleBotInit();
   bot.sendMessage(myTele, "Бот запущен: "+IP_to_String(WiFi.localIP()), "");
+  mLog("Bep-belep...");
   beep(125,50);
    delay(50);
   beep(125,50); 
-
+  mLog("Let's do it.");
 }
 
 void loop() {
+
   goBot();
   httpServer.handleClient();
   clok();
--- src/myIR.h
+++ src/myIR.h
@@ -1,6 +1,24 @@
 #ifndef myIR
 #define myIR
-#include "IRremoteint.h"
+//#include "IRremoteint.h"
+#include <IRremoteESP8266.h> 
+#ifndef common
+  #include "common.h"
+ #endif
+int RECV_PIN = 2; //an IR detector/demodulatord is connected to GPIO pin 2 
 
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void MyIRInit(void){
+    irrecv.enableIRIn(); // Start the receiver
+ }
+
+void MyIRWork(void){
+    if (irrecv.decode(&results)) {
+    mLog(String(results.value));//, HEX);
+    irrecv.resume(); // Receive the next value
+  } 
+ }
 
 #endif
